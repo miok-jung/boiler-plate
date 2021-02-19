c513251635e6ab16aaae85897680e81caebfac38
miok-jung <miok.dev@gmail.com>
2021-02-19 16:53:57 +0900

    MongoDB Connect

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 yarn run start
 - package.json에서 script에 의해 node index.js를 실행할 수 있게 설정.
 - index.js에서는 express기반으로 5000포트에서 index.js를 실행한다. 실행시 res.send에 대한 내용이 표출이 된다.
+
+21. 02. 19
+MongoDB 연결
+- yarn run start에 대한 오류로 인해 mongodb패키지 추가 및 mongoose다운그레이 진행하여 오류는 없음
+- 콘솔창에 db연결이 잘되었는지 확인여부가 안되는 상황
--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000 // 벡서버
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://miok:tpzy591@boilerplate.7efpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 미사용시 에러발생
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+  
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
 })
